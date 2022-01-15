06368e17dd30ae0c409833753f18d3239da3b20a
Danila000Ryabinin <97749908+Danila000Ryabinin@users.noreply.github.com>
2022-01-15 14:25:36 +0300

    Delete fact.hs

--- fact.hs
+++
@@ -1,3 +0,0 @@
-doublefact :: Integer -> Integer
-doublefact n = if n <= 0 then error "Вы ввели отрицательное число" else if mod n 2 > 0 then pr(n-1) else pr(n)
-pr n = if n <=0 then 1 else pr(n-2)*n
